e5b152d6647e30df556437895836a8a83f0ec115
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-07-25 15:50:49 +0200

    HomePage Done

--- app-front-end/src/pages/Home/index.jsx
+++ app-front-end/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Banner from "../../components/Banner";
+import Features from "../../components/Features";
 
 function Home() {
   return (
     <div className="home-container">
       
       <Banner></Banner>
+      <Features></Features>
 
     </div>
   );
