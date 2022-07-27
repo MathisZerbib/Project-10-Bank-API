f5ea0ade355cc6b7067dfe14037658aef902f367
Mathis Zerbib <mathis.zerbib@gmail.com>
2022-07-27 22:04:24 +0200

    clean nav + fa OpenDoor

--- app-front-end/src/components/Nav/Nav.jsx
+++ app-front-end/src/components/Nav/Nav.jsx
@@ -1,13 +1,14 @@
-import { NavLink } from 'react-router-dom';
-import { useSelector, useDispatch } from 'react-redux';
+import { NavLink } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
 import styled from "styled-components";
 import ArgentBankLogo from "../../assets/images/argent-bank-logo.png";
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { faUserCircle } from "@fortawesome/free-solid-svg-icons";
+import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
 
-import { authActions } from '../../_store';
+import { authActions } from "../../_store";
 
 const NavContainer = styled.div`
   display: flex;
@@ -28,62 +29,58 @@ const HomeLogo = styled.img`
 //   justify-content: space-evenly;
 // `;
 
-
-
 export { Nav };
 
 function Nav() {
-    const authUser = useSelector(x => x.auth.user);
-    const dispatch = useDispatch();
-    const logout = () => dispatch(authActions.logout());
+  const authUser = useSelector((x) => x.auth.user);
+  const dispatch = useDispatch();
+  const logout = () => dispatch(authActions.logout());
 
-    // only show nav when logged in
-    if (!authUser) 
+  // only show nav when logged in
+  if (!authUser)
     return (
+      <div>
         <NavContainer className="main-nav">
-           <div className="navbar-nav">
-                 <NavLink to={"/"}>
-       <HomeLogo src={ArgentBankLogo} />
-       <h1 className="sr-only">Argent Bank</h1>
-     </NavLink>                
-
-     <NavLink to={"/login"}>
-         <FontAwesomeIcon icon={faUserCircle} />
-         Sign-In
-       </NavLink>
-       </div>
-       </NavContainer>
-    );
-    
-    return (
-         <NavContainer className="main-nav">
-            <div className="navbar-nav">
-                  <NavLink to={"/"}>
-        <HomeLogo src={ArgentBankLogo} />
-        <h1 className="sr-only">Argent Bank</h1>
-      </NavLink>                
-      
-      <button onClick={logout} className="btn btn-link nav-item nav-link">Logout</button>
-            </div>
+          <div className="navbar-nav">
+            <NavLink to={"/"}>
+              <HomeLogo src={ArgentBankLogo} />
+              <h1 className="sr-only">Argent Bank</h1>
+            </NavLink>
+          </div>
         </NavContainer>
-        
-// <div>
-// <NavContainer className="main-nav">
-//       <NavLink to={"/"}>
-//         <HomeLogo src={ArgentBankLogo} />
-//         <h1 className="sr-only">Argent Bank</h1>
-//       </NavLink>
-      
-//     <div>
-//       <LinkContainer className="main-nav-item">
-//         <NavLink className={isLinkActive} to={"/login"}>
-//           <FontAwesomeIcon icon={faUserCircle} />
-//           Sign-In
-//         </NavLink>
-//       </LinkContainer>
-//     </div>
-//   </NavContainer>
-//     </div>
 
+        <NavContainer>
+          <NavLink to={"/login"}>
+            <FontAwesomeIcon icon={faUserCircle} />
+            Sign-In
+          </NavLink>
+        </NavContainer>
+      </div>
     );
+
+  return (
+    <div>
+      <NavContainer className="main-nav">
+        <div className="navbar-nav">
+          <NavLink to={"/"}>
+            <HomeLogo src={ArgentBankLogo} />
+            <h1 className="sr-only">Argent Bank</h1>
+          </NavLink>
+        </div>
+      </NavContainer>
+      <NavContainer>
+          <NavLink to={"/user"}>
+            <p>{authUser.firstName}</p>
+            <FontAwesomeIcon icon={faUserCircle} />
+          </NavLink>
+
+        <div>
+          <FontAwesomeIcon icon={faDoorOpen} />
+          <button onClick={logout} className="btn btn-link nav-item nav-link">
+            Logout
+          </button>
+        </div>
+      </NavContainer>
+    </div>
+  );
 }
